dfc545d555b4d773803ed8b49148ecc39ad66104
LYU Peiduo <20075754d@msds.comp.polyu.edu.hk>
2024-01-12 00:02:44 +0800

    some update

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,58 @@
 use std::arch::x86_64::*;
 
-fn matmul_simd(a: &[f32], b: &[f32], c: &mut [f32], rows_a: usize, cols_a: usize, cols_b: usize) {
-    assert_eq!(a.len(), rows_a * cols_a);
-    assert_eq!(b.len(), cols_a * cols_b);
-    assert_eq!(c.len(), rows_a * cols_b);
+fn kernel(a: &[f32], b: &[f32], c: &mut [f32], x: usize, y: usize, l: usize, r: usize, n: usize, m: usize) {
 
-    let mut i = 0;
-    while i < rows_a {
-        let mut j = 0;
-        while j < cols_b {
-            let mut k = 0;
-            while k < cols_a {
-                unsafe {
-                    let a_ptr = a.as_ptr().add(i * cols_a + k);
-                    let b_ptr = b.as_ptr().add(k * cols_b + j);
-                    let c_ptr = c.as_mut_ptr().add(i * cols_b + j);
+    unsafe {
+        let mut t: [[__m256; 2]; 6] = [[_mm256_setzero_ps(); 2]; 6];
+        for k in l..r {
+            let b0 = _mm256_load_ps(&b[k * n + y]);
+            let b1 = _mm256_load_ps(&b[k * n + y + 8]);
+            for i in 0..6 {
+                let alpha = _mm256_broadcast_ss(&a[(x + i) * m + k]);
 
-                    let a_val = *a_ptr;
-                    let a_broadcast = _mm256_set1_ps(a_val);
-
-                    let b_vec = _mm256_loadu_ps(b_ptr);
-                    let c_vec = _mm256_loadu_ps(c_ptr);
-
-                    let result = _mm256_add_ps(_mm256_mul_ps(a_broadcast, b_vec), c_vec);
-
-                    _mm256_storeu_ps(c_ptr, result);
-                }
-                k += 1;
+                t[i][0] = _mm256_fmadd_ps(alpha, b0, t[i][0]);
+                t[i][1] = _mm256_fmadd_ps(alpha, b1, t[i][1]);
             }
-            j += 16;
         }
-        i += 6;
+    
+
+        for i in 0..6 {
+            for j in 0..2 {
+                let mut temp = [0.0; 8];
+                _mm256_store_ps(temp.as_mut_ptr(), t[i][j]);
+                for k in 0..8 {
+                    c[((x + i) * n + y) / 8 + j] += temp[k];
+                }
+            }
+        }
     }
 }
 
-fn main() {
-    let rows_a = 6;
-    let cols_a = 4;
-    let cols_b = 8;
+fn matmul(a: &[f32], b: &[f32], c: &mut [f32], n: usize, m: usize) {
+    let nx = (n + 5) / 6 * 6;
+    let ny = (m + 15) / 16 * 16;
+    
+    let mut a_padded = vec![0.0; nx * ny];
+    let mut b_padded = vec![0.0; nx * ny];
+    let mut c_padded = vec![0.0; nx * ny];
 
-    let a = vec![1.0, 2.0, 3.0, 4.0,
-                 5.0, 6.0, 7.0, 8.0,
-                 9.0, 10.0, 11.0, 12.0,
-                 13.0, 14.0, 15.0, 16.0,
-                 17.0, 18.0, 19.0, 20.0,
-                 21.0, 22.0, 23.0, 24.0];
-    let b = vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0,
-                 9.0, 10.0, 11.0, 12.0, 13.0, 14.0, 15.0, 16.0,
-                 17.0, 18.0, 19.0, 20.0, 21.0, 22.0, 23.0, 24.0,
-                 25.0, 26.0, 27.0, 28.0, 29.0, 30.0, 31.0, 32.0];
-    let mut c = vec![0.0; rows_a * cols_b];
+    for i in 0..n {
+        a_padded[i * ny..(i + 1) * ny].copy_from_slice(&a[i * m..(i + 1) * m]);
+    }
 
-    matmul_simd(&a, &b, &mut c, rows_a, cols_a, cols_b);
+    for i in 0..m {
+        b_padded[i * ny..(i + 1) * ny].copy_from_slice(&b[i * n..(i + 1) * n]);
+    }
 
-    println!("{:?}", c);
+    for x in (0..nx).step_by(6) {
+        for y in (0..ny).step_by(16) {
+            kernel(&a_padded, &b_padded, &mut c_padded, x, y, 0, m, ny, n);
+        }
+    }
+
+    for i in 0..n {
+        c[i * m..(i + 1) * m].copy_from_slice(&c_padded[i * ny..(i + 1) * ny]);
+    }
 }
+
+
